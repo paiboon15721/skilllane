76adeb03a500c1d9a2ea6af4b101bd5ba09f21a1
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-10-25 13:29:05 +0700

    update style

--- src/components/ChatRoom.tsx
+++ src/components/ChatRoom.tsx
@@ -3,14 +3,14 @@ import React from 'react'
 interface Props {}
 
 const Comp: React.FC<Props> = () => (
-  <>
+  <div className="pl-4">
     <h5>
       <i className="fa fa-comments"></i> ห้องสนทนา
     </h5>
     <p className="text-muted">
       กรุณาลงทะเบียนเรียนคอร์สนี้เพื่อใช้งานห้องสนทนา
     </p>
-  </>
+  </div>
 )
 
 export default Comp
