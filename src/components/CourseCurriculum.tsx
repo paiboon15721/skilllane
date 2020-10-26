10b6b5a7f9c2fca4909bf1fb120a504052d32ccb
Paiboon Auengkongkatong <paiboon15721@gmail.com>
2020-10-26 10:35:12 +0700

    add course curriculum components

--- src/components/CourseCurriculum.tsx
+++ src/components/CourseCurriculum.tsx
@@ -1,16 +1,39 @@
 import React from 'react'
+import curriculums from '../data/curriculums'
 
 interface Props {}
 
 const Comp: React.FC<Props> = () => (
-  <div className="card">
+  <div className="card course-curriculum-card">
     <div className="card-header">เนื้อหาของคอร์สนี้</div>
     <div className="card-body">
-      <h5 className="card-title">Primary card title</h5>
-      <p className="card-text">
-        Some quick example text to build on the card title and make up the bulk
-        of the card's content.
-      </p>
+      {curriculums.map(v => (
+        <div className="section" key={v.chapter}>
+          <div className="chapter">
+            <h5>{v.chapter}</h5>
+          </div>
+          <div className="videos">
+            {v.videos.map(x => (
+              <div key={x.name} className="video">
+                <p className="name">{x.name}</p>
+                <div className="detail">
+                  <div className="icons">
+                    {x.free && (
+                      <button className="btn btn-sm btn-primary">
+                        ดูตัวอย่างฟรี
+                      </button>
+                    )}
+                    {x.icons.map(i => (
+                      <i key={i} className={`${i} fa-lg`}></i>
+                    ))}
+                  </div>
+                  <p>{x.time}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   </div>
 )
---
+++ src/data/curriculums.tsx
@@ -0,0 +1,242 @@
+export default [
+  {
+    chapter: 'บทที่ 1 สิ่งที่จำเป็นต้องรู้ก่อนลงมือสร้างเว็บ',
+    videos: [
+      {
+        name: '1.1 คำแนะนำการเรียนรู้ให้ได้ผล',
+        free: false,
+        icons: ['fa fa-book-open'],
+        time: '1 นาที',
+      },
+      {
+        name: '1.2 ต้นทุนสำหรับการสร้างเว็บไซต์ด้วย WordPress มีเท่าไหร่',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '14:24',
+      },
+      {
+        name: '1.3 ทำไมต้องรู้จักการทำ SEO ด้วย',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '08:43',
+      },
+      {
+        name:
+          '1.4 ทำความรู้จัก WordPress และรู้จักองค์ประกอบของการสร้างเว็บด้วย WordPress',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '20:31',
+      },
+    ],
+  },
+  {
+    chapter: 'บทที่ 1 สิ่งที่จำเป็นต้องรู้ก่อนลงมือสร้างเว็บ',
+    videos: [
+      {
+        name: '1.1 คำแนะนำการเรียนรู้ให้ได้ผล',
+        free: false,
+        icons: ['fa fa-book-open'],
+        time: '1 นาที',
+      },
+      {
+        name: '1.2 ต้นทุนสำหรับการสร้างเว็บไซต์ด้วย WordPress มีเท่าไหร่',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '14:24',
+      },
+      {
+        name: '1.3 ทำไมต้องรู้จักการทำ SEO ด้วย',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '08:43',
+      },
+      {
+        name:
+          '1.4 ทำความรู้จัก WordPress และรู้จักองค์ประกอบของการสร้างเว็บด้วย WordPress',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '20:31',
+      },
+    ],
+  },
+  {
+    chapter: 'บทที่ 1 สิ่งที่จำเป็นต้องรู้ก่อนลงมือสร้างเว็บ',
+    videos: [
+      {
+        name: '1.1 คำแนะนำการเรียนรู้ให้ได้ผล',
+        free: false,
+        icons: ['fa fa-book-open'],
+        time: '1 นาที',
+      },
+      {
+        name: '1.2 ต้นทุนสำหรับการสร้างเว็บไซต์ด้วย WordPress มีเท่าไหร่',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '14:24',
+      },
+      {
+        name: '1.3 ทำไมต้องรู้จักการทำ SEO ด้วย',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '08:43',
+      },
+      {
+        name:
+          '1.4 ทำความรู้จัก WordPress และรู้จักองค์ประกอบของการสร้างเว็บด้วย WordPress',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '20:31',
+      },
+    ],
+  },
+  {
+    chapter: 'บทที่ 1 สิ่งที่จำเป็นต้องรู้ก่อนลงมือสร้างเว็บ',
+    videos: [
+      {
+        name: '1.1 คำแนะนำการเรียนรู้ให้ได้ผล',
+        free: false,
+        icons: ['fa fa-book-open'],
+        time: '1 นาที',
+      },
+      {
+        name: '1.2 ต้นทุนสำหรับการสร้างเว็บไซต์ด้วย WordPress มีเท่าไหร่',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '14:24',
+      },
+      {
+        name: '1.3 ทำไมต้องรู้จักการทำ SEO ด้วย',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '08:43',
+      },
+      {
+        name:
+          '1.4 ทำความรู้จัก WordPress และรู้จักองค์ประกอบของการสร้างเว็บด้วย WordPress',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '20:31',
+      },
+    ],
+  },
+  {
+    chapter: 'บทที่ 1 สิ่งที่จำเป็นต้องรู้ก่อนลงมือสร้างเว็บ',
+    videos: [
+      {
+        name: '1.1 คำแนะนำการเรียนรู้ให้ได้ผล',
+        free: false,
+        icons: ['fa fa-book-open'],
+        time: '1 นาที',
+      },
+      {
+        name: '1.2 ต้นทุนสำหรับการสร้างเว็บไซต์ด้วย WordPress มีเท่าไหร่',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '14:24',
+      },
+      {
+        name: '1.3 ทำไมต้องรู้จักการทำ SEO ด้วย',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '08:43',
+      },
+      {
+        name:
+          '1.4 ทำความรู้จัก WordPress และรู้จักองค์ประกอบของการสร้างเว็บด้วย WordPress',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '20:31',
+      },
+    ],
+  },
+  {
+    chapter: 'บทที่ 1 สิ่งที่จำเป็นต้องรู้ก่อนลงมือสร้างเว็บ',
+    videos: [
+      {
+        name: '1.1 คำแนะนำการเรียนรู้ให้ได้ผล',
+        free: false,
+        icons: ['fa fa-book-open'],
+        time: '1 นาที',
+      },
+      {
+        name: '1.2 ต้นทุนสำหรับการสร้างเว็บไซต์ด้วย WordPress มีเท่าไหร่',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '14:24',
+      },
+      {
+        name: '1.3 ทำไมต้องรู้จักการทำ SEO ด้วย',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '08:43',
+      },
+      {
+        name:
+          '1.4 ทำความรู้จัก WordPress และรู้จักองค์ประกอบของการสร้างเว็บด้วย WordPress',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '20:31',
+      },
+    ],
+  },
+  {
+    chapter: 'บทที่ 1 สิ่งที่จำเป็นต้องรู้ก่อนลงมือสร้างเว็บ',
+    videos: [
+      {
+        name: '1.1 คำแนะนำการเรียนรู้ให้ได้ผล',
+        free: false,
+        icons: ['fa fa-book-open'],
+        time: '1 นาที',
+      },
+      {
+        name: '1.2 ต้นทุนสำหรับการสร้างเว็บไซต์ด้วย WordPress มีเท่าไหร่',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '14:24',
+      },
+      {
+        name: '1.3 ทำไมต้องรู้จักการทำ SEO ด้วย',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '08:43',
+      },
+      {
+        name:
+          '1.4 ทำความรู้จัก WordPress และรู้จักองค์ประกอบของการสร้างเว็บด้วย WordPress',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '20:31',
+      },
+    ],
+  },
+  {
+    chapter: 'บทที่ 1 สิ่งที่จำเป็นต้องรู้ก่อนลงมือสร้างเว็บ',
+    videos: [
+      {
+        name: '1.1 คำแนะนำการเรียนรู้ให้ได้ผล',
+        free: false,
+        icons: ['fa fa-book-open'],
+        time: '1 นาที',
+      },
+      {
+        name: '1.2 ต้นทุนสำหรับการสร้างเว็บไซต์ด้วย WordPress มีเท่าไหร่',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '14:24',
+      },
+      {
+        name: '1.3 ทำไมต้องรู้จักการทำ SEO ด้วย',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '08:43',
+      },
+      {
+        name:
+          '1.4 ทำความรู้จัก WordPress และรู้จักองค์ประกอบของการสร้างเว็บด้วย WordPress',
+        free: true,
+        icons: ['fa fa-play-circle'],
+        time: '20:31',
+      },
+    ],
+  },
+]
